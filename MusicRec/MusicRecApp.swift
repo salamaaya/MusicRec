131bab4058144a9cacf0eaece1c72b165487fcaf
ayasalama <asalama0204@gmail.com>
2023-08-25 17:37:44 -0400

    Initial Commit

---
+++ MusicRec/MusicRecApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicRecApp.swift
+//  MusicRec
+//
+//  Created by Aya Salama on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicRecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
